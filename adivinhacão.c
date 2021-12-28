0687c0bcf1db92069d12167b18e153dd91ebd68a
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2021-12-28 11:23:43 -0300

    adicionando níveis de dificuldade com switch

--- adivinhacão.c
+++ adivinhacão.c
@@ -4,16 +4,39 @@
 
 int
 main(void){
+
     double points = 1000;
+    int level;
     int tries = 0;
     int won = 0;
     int kick;
-    int Maxtries;
+    int maxTries;
 
     srand(time(NULL));
     int randomNumber = rand() % 100;
 
     printf("Seja bem-vinde ao jogo de adivinhação! Acerte o número aleatório e vença o jogo! \n");
+
+    printf("Escolha seu nível de dificuldade [1]fácil [3]médio [5]difícil\n");
+    scanf("%d", &level);
+
+    switch (level)
+    {
+    case 1:
+        maxTries = 20;
+        break;
+
+    case 3:
+        maxTries = 16;
+        break;
+
+    case 5:
+        maxTries = 6;
+
+    default:
+        maxTries = 6;
+        break;
+    }
     
 
     do{
@@ -42,7 +65,7 @@ main(void){
             tries++;
         }
 
-    } while (won == 0);
+    } while (won == 0 && tries <= maxTries);
     
     
 }
