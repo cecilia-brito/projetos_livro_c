5d92828ef29a56051b97d2fc50934e19d34fc66e
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2021-12-28 10:47:15 -0300

    adicionando condições para testar o chute do usuário

--- adivinhacão.c
+++ adivinhacão.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+int
+main(void){
+    int points = 1000;
+    int tries = 0;
+    int won = 0;
+    int kick;
+
+    srand(time(NULL));
+    int randomNumber = rand() % 100;
+
+    printf("Seja bem-vinde ao jogo de adivinhação! Acerte o número aleatório e vença o jogo! \n");
+    
+
+    while(!won){
+        scanf("Digite seu chute: %d \n", kick);
+        if(kick == randomNumber){
+            printf("Parabéns você ganhou!");
+            break;
+        } else if(kick > randomNumber) {
+            printf("O número que você chutou é muito alto!\n");
+        } else{
+            printf("O número que você chutou é baixo!\n");
+        }
+    }
+    
+}
