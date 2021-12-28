fc77b0f9058ab9217952a0a6ad02e612a3a16f14
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2021-12-28 11:07:08 -0300

    adicionando contagem de pontos

--- adivinhacão.c
+++ adivinhacão.c
@@ -4,10 +4,11 @@
 
 int
 main(void){
-    int points = 1000;
+    double points = 1000;
     int tries = 0;
     int won = 0;
     int kick;
+    int Maxtries;
 
     srand(time(NULL));
     int randomNumber = rand() % 100;
@@ -15,16 +16,33 @@ main(void){
     printf("Seja bem-vinde ao jogo de adivinhação! Acerte o número aleatório e vença o jogo! \n");
     
 
-    while(!won){
-        scanf("Digite seu chute: %d \n", kick);
-        if(kick == randomNumber){
-            printf("Parabéns você ganhou!");
+    do{
+        printf("Qual é seu chute?\n");
+        scanf("%d", &kick);
+        
+        if(kick <0){
+            printf("Você não pode chutar números negativos! Tente novamente\n");
+            tries--;
+        } else if (kick == randomNumber){
+            won = 1;
+            printf("Parabéns você ganhou!\n");
+            points -= (double) ((abs(kick) - randomNumber)/2);
+            tries++;
+            printf("Seus pontos foram: %.1f", points);
             break;
+
         } else if(kick > randomNumber) {
             printf("O número que você chutou é muito alto!\n");
+            points -= (double) ((abs(kick) - randomNumber)/2);
+            tries++;
+
         } else{
             printf("O número que você chutou é baixo!\n");
+            points -= (double) ((abs(kick) - randomNumber)/2);
+            tries++;
         }
-    }
+
+    } while (won == 0);
+    
     
 }
